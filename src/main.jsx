edd49a4472dfc5a963c98b0b9b0a6851233fe9ec
pccmymas <pccmymas@gmail.com>
2026-05-31 13:55:31 -0600

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import Landing from './Landing.jsx'
+import PerfilTaller from './PerfilTaller.jsx'
 
-const isApp = window.location.pathname.startsWith('/app');
+const path = window.location.pathname;
+const isApp    = path.startsWith('/app');
+const isTaller = path.startsWith('/taller/');
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    {isApp ? <App /> : <Landing />}
+    {isApp ? <App /> : isTaller ? <PerfilTaller /> : <Landing />}
   </StrictMode>,
 )
