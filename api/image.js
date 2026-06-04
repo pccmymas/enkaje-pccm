4babb58d1308e49d1ea0c8e25d4c2fd984c1ed60
pccmymas <pccmymas@gmail.com>
2026-06-04 14:52:49 -0600

    Set body parser size limit to 10mb

--- api/image.js
+++ api/image.js
@@ -68,3 +68,10 @@ export default async function handler(req, res) {
     return res.status(200).json({ error: err.message });
   }
 }
+export const config = {
+  api: {
+    bodyParser: {
+      sizeLimit: "10mb",
+    },
+  },
+};
