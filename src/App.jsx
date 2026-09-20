35b29d9970efc16668d2db488baaca0457d65fcb
pccmymas <pccmymas@gmail.com>
2026-09-20 16:48:30 -0600

    Add zonas_cobertura to tallerEdit update fields

--- src/App.jsx
+++ src/App.jsx
@@ -1800,6 +1800,7 @@ async function guardarEdicionTaller() {
       instagram: tallerEdit.instagram, facebook: tallerEdit.facebook, tiktok: tallerEdit.tiktok,
       logo_url: tallerEdit.logo_url, portafolio_urls: tallerEdit.portafolio_urls,
       niveles_precio: tallerEdit.niveles_precio,
+      zonas_cobertura: tallerEdit.zonas_cobertura,
     };
     const limpio = Object.fromEntries(Object.entries(campos).filter(([,v]) => v !== undefined && v !== null && v !== ""));
     await sb(`talleres_membresia?id=eq.${tallerEdit.id}`, { method: "PATCH", token, body: JSON.stringify(limpio) });
